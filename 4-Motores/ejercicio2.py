12960269b5ef40ddb0df612fd93ac21d9e942f2f
DIturriaga <diturriaga@mty-laptop182.local>
2022-05-04 18:17:53 -0500

    Add Servo Code

--- 4-Motores/ejercicio2.py
+++ 4-Motores/ejercicio2.py
@@ -3,28 +3,33 @@ Ejercicio2 - Escribe un programa en tu Raspberry que simulé el funcionamiento d
 by: David Iturriaga Sotelo
 """
 
-import RPi.GPIO as GPIO
-import time
+from gpiozero import AngularServo
+from guizero import App, ButtonGroup, Text
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+app=App(title="Servo_GUI", layout="grid")
+s = AngularServo(17, min_angle=-90, max_angle=90)
+s1 = AngularServo(23, min_angle=-90, max_angle=90)
+s2 = AngularServo(24, min_angle=-90, max_angle=90)
+s3 = AngularServo(4, min_angle=-90, max_angle=90)
 
-GPIO.setup(12, GPIO.OUT)
+def update_text():
+    s.angle = int(choice.value)
+    print("motor1: ", s.angle)
+    s1.angle = int(choice1.value)
+    print("motor2: ",s1.angle)
+    s2.angle = int(choice2.value)
+    print("motor3: ",s2.angle)
+    s3.angle = int(choice3.value)
+    print("motor4: ",s3.angle, "\n------------------------------------------------")
 
-LED = GPIO.PWM(12, 50)
-duty = 0 
-LED.start(duty)
+choice =ButtonGroup(app, options=["-90", "-45", "0", "45", "90"], selected="-90",command=update_text, grid=[1,2])
+choice1 =ButtonGroup(app, options=["-90", "-45", "0", "45", "90"], selected="-90", command=update_text, grid=[2,2])
+choice2 =ButtonGroup(app, options=["-90", "-45", "0", "45", "90"], selected="-90", command=update_text, grid=[3,2])
+choice3 =ButtonGroup(app, options=["-90", "-45", "0", "45", "90"], selected="-90", command=update_text, grid=[4,2])
 
-try:
-	while True:
-		duty += 5
-		if duty > 100:
-			duty = 0
-		LED.ChangeDutyCycle(duty)
-		time(.05)
-except KeyboardInterrupt:
-	print("Fin")
-
-LED.stop()
-GPIO.cleanup()
+Text(app, "M1",grid=[1,1])
+Text(app, "M2",grid=[2,1])
+Text(app, "M3",grid=[3,1])
+Text(app, "M4",grid=[4,1])
+app.display()
 
