4b08f757d812c9c7cc15c6c4af826f6c3d7d4f73
David Iturriaga <iturriaga_david@hotmail.com>
2022-05-05 09:58:25 -0500

    Add Servo PWM

--- 4-Motores/ejercicio2.py
+++ 4-Motores/ejercicio2.py
@@ -2,16 +2,33 @@
 Ejercicio2 - Escribe un programa en tu Raspberry que simulé el funcionamiento de un robot Araña.
 by: David Iturriaga Sotelo
 """
-
+import RPi.GPIO as GPIO
+import time
 from gpiozero import AngularServo
-from guizero import App, ButtonGroup, Text
+#from guizero import App, ButtonGroup, Text
 
-app=App(title="Servo_GUI", layout="grid")
-s = AngularServo(17, min_angle=-90, max_angle=90)
+#app=App(title="Servo_GUI", layout="grid")
+
+GPIO.setwarnings(False)
+
+s = AngularServo(12, min_angle=-90, max_angle=90)
 s1 = AngularServo(23, min_angle=-90, max_angle=90)
 s2 = AngularServo(24, min_angle=-90, max_angle=90)
 s3 = AngularServo(4, min_angle=-90, max_angle=90)
 
+while True:
+    s.angle = int(-90)
+    time.sleep(1)
+    s1.angle = int(-90)
+    s2.angle = int(-90)
+    time.sleep(1)
+    s.angle = int(90)
+    time.sleep(1)
+    s1.angle = int(90)
+    s2.angle = int(90)
+    time.sleep(1)
+    
+    
 def update_text():
     s.angle = int(choice.value)
     print("motor1: ", s.angle)
@@ -22,14 +39,14 @@ def update_text():
     s3.angle = int(choice3.value)
     print("motor4: ",s3.angle, "\n------------------------------------------------")
 
-choice =ButtonGroup(app, options=["-90", "-45", "0", "45", "90"], selected="-90",command=update_text, grid=[1,2])
-choice1 =ButtonGroup(app, options=["-90", "-45", "0", "45", "90"], selected="-90", command=update_text, grid=[2,2])
-choice2 =ButtonGroup(app, options=["-90", "-45", "0", "45", "90"], selected="-90", command=update_text, grid=[3,2])
-choice3 =ButtonGroup(app, options=["-90", "-45", "0", "45", "90"], selected="-90", command=update_text, grid=[4,2])
+#choice =ButtonGroup(app, options=["-90", "-45", "0", "45", "90"], selected="-90",command=update_text, grid=[1,2])
+#choice1 =ButtonGroup(app, options=["-90", "-45", "0", "45", "90"], selected="-90", command=update_text, grid=[2,2])
+#choice2 =ButtonGroup(app, options=["-90", "-45", "0", "45", "90"], selected="-90", command=update_text, grid=[3,2])
+#choice3 =ButtonGroup(app, options=["-90", "-45", "0", "45", "90"], selected="-90", command=update_text, grid=[4,2])
 
-Text(app, "M1",grid=[1,1])
-Text(app, "M2",grid=[2,1])
-Text(app, "M3",grid=[3,1])
-Text(app, "M4",grid=[4,1])
-app.display()
+#Text(app, "M1",grid=[1,1])
+#Text(app, "M2",grid=[2,1])
+#Text(app, "M3",grid=[3,1])
+#Text(app, "M4",grid=[4,1])
+#app.display()
 
