c90173ae4d8e324beeb1c19a7264a8f3405b07c4
David Iturriaga Sotelo <alfredo.reyes@sat.gob.mx>
2022-05-19 12:34:49 -0500

    Add missing ejercicio2

--- 6-Sensores/ejercicio2/arduino/ejercicio2.ino
+++ 6-Sensores/ejercicio2/arduino/ejercicio2.ino
@@ -3,3 +3,32 @@ Ejercicio 2 (Sensor Ultrasónico)
 Escribe un programa en tu Raspberry que detecte la distancia, la distancia debe de ser mostrada en un LCD.
 by: David Iturriaga Sotelo
 """
+
+long dis;
+long tiem;
+int led1 = 11;
+void setup( ){
+Serial.begin(9600);
+pinMode(10, OUTPUT); //salida del pulso generado por el sensor ultrasónico
+pinMode(9, INPUT);//entrada del pulso generado por el sensor ultrasónico
+pinMode(11, INPUT);//alarma de la distancia(encenderá el led)
+}
+void loop(){
+digitalWrite(10,LOW);//recibimiento del pulso.
+delayMicroseconds(5);
+digitalWrite(10, HIGH);//envió del pulso.
+delayMicroseconds(10);
+tiem=pulseIn(9, HIGH);//fórmula para medir el pulso entrante.
+dis= long(0.017*tiem);//fórmula para calcular la distancia del sensor ultrasónico.
+if(dis>10){ //comparativo para la alarma se ingresa la distancia en la que encenderá o apagara.
+digitalWrite(led1, HIGH);
+}
+else
+{
+digitalWrite(11,LOW);
+}
+Serial.println("LA DISTANCIA MEDIDA ES:");
+Serial.println(dis);
+Serial.println("cm");
+delay(500);
+}
